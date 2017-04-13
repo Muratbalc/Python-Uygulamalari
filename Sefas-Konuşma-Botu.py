e4d9256a9223613b2b8ae64184f24fa85619bc39
Murat Balcı <canbazmurat785@gmail.com>
2017-04-13 12:25:21 +0300

    Update Sefas-Konuşma-Botu.py

--- Sefas-Konuşma-Botu.py
+++ Sefas-Konuşma-Botu.py
@@ -1,4 +1,5 @@
 # -*- coding: cp1254 -*-  #Windows için Türkçe karakter dil kodlamasını belli ettik
+from __future__  import division
 
 print "Benim ismim Sefas " 
 print "Merhaba İsmini öğrenebilirmiyim ?"
@@ -6,7 +7,8 @@ isim = raw_input("İsmin nedir: ")
 print "Demek ismin",isim,"'hımm...."
 print "Tanıştığımıza Memnun oldum",isim
 cevap = raw_input("eee nasılsın"+isim+" (Evet iyiyim[e]/Hayır iyi değilim [h])")
-cevap2 = "" #if koşulluna doğrudan cevap2 = raw_input("sadas") yazsaydık hata verecekti bizde ilk olarak cevap2'yi burada belirttik
+cevap2 = ""
+drs =""
 if cevap == "e":   
                 print "Bunu duyduğum iyi oldu :D"
 if cevap == "h":
@@ -14,9 +16,108 @@ if cevap == "h":
 if cevap2 == "e": 
                 print "Anlat Dinliyorum"
                 raw_input("")
-                print "Neyse bu kötü olmuş",isim,"ben gidiyorum kendine iyi bak"
+                drs=raw_input("Ders ortalamanı alim mi ?[e/h]")
+if drs=="e":
+                ders1 ="[1] Türkçe"
+                ders2 ="[2] Matematik"
+                ders3 ="[3] Fen Bilgisi"
+                ders4 ="[4] İnkılap"
+                ders5 ="[5] İngilizce"
+
+                print ders1
+                print ders2
+                print ders3
+                print ders4
+                print ders5
+
+                ders = int(raw_input("Ders Seçin (1-5):"))
+
+                if ders == 1:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:",no/3
+                if ders == 2:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:", no/3
+                if ders == 3:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:", no/3
+                if ders == 4:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                no=nb+ni
+                                print "Ortalamanız:", no/2
+                if ders == 5:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:", no/3
+                if ders > 5:
+                                print "Ders Seçmediniz Lütfen Ders Seçin !!!(Uygulama Kapanacaktır)"
+                if ders < 1:
+
+                                print "Ders Seçmediniz Lütfen Ders Seçin !!!(Uygulama Kapanacaktır)"
 if cevap2 == "h":
                 print "Peki seni üzen nedir ?"
                 raw_input("")
-                print "Neyse bu kötü olmuş ben gidiyorum kendine iyi bak"
+                drs=raw_input("Ders ortalamanı alim mi ?[e/h]")
+if drs=="e":
+                ders1 ="[1] Türkçe"
+                ders2 ="[2] Matematik"
+                ders3 ="[3] Fen Bilgisi"
+                ders4 ="[4] İnkılap"
+                ders5 ="[5] İngilizce"
+
+                print ders1
+                print ders2
+                print ders3
+                print ders4
+                print ders5
+
+                ders = int(raw_input("Ders Seçin (1-5):"))
+
+                if ders == 1:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:",no/3
+                if ders == 2:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:", no/3
+                if ders == 3:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:", no/3
+                if ders == 4:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                no=nb+ni
+                                print "Ortalamanız:", no/2
+                if ders == 5:
+                                nb = int(raw_input("İlk Ders Notu:"))
+                                ni = int(raw_input("İkinci ders Notu: "))
+                                nu = int(raw_input("Üçüncü Ders Notu:"))
+                                no=nb+ni+nu 
+                                print "Ortalamanız:", no/3
+                if ders > 5:
+                                print "Ders Seçmediniz Lütfen Ders Seçin !!!(Uygulama Kapanacaktır)"
+                if ders < 1:
+
+                                print "Ders Seçmediniz Lütfen Ders Seçin !!!(Uygulama Kapanacaktır)"
                 
+
